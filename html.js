ae797ff6bb27e490baf9e0fd07b0794bd56cd852
Mark <nixmrak@yandex.ru>
2013-11-23 00:11:59 +0200

    html

---
+++ html.js
@@ -0,0 +1,52 @@
+var stream = require('stream');
+var util = require('util');
+
+function HTMLStream() {
+    stream.Transform.call(this, {
+        objectMode: true
+    });
+
+};
+
+util.inherits(HTMLStream, stream.Transform);
+
+HTMLStream.prototype._transform = function(chunk, encoding, done) {
+    if (chunk.type === 'p') {
+        this._pushPara(chunk);
+    } else if (chunk.type === 'h') {
+        this._pushTitle(chunk);
+    }
+    done(null);
+};
+
+HTMLStream.prototype._pushPara = function(chunk) {
+    this.push('<p>' + markupToString(chunk.children) + '</p>\n');
+}
+HTMLStream.prototype._pushTitle = function(chunk) {
+    var level = chunk.level + 1;
+    if (level > 6) {
+        level = 6;
+    }
+    this.push('<h' + level + '>' + markupToString(chunk.children) + '</h' + level + '>\n');
+}
+
+HTMLStream.prototype._flush = function() {
+    this.push(null);
+};
+
+module.exports = HTMLStream;
+
+function markupToString(elements) {
+    return elements.map(function(el) {
+        if (el.type === 'text') {
+            return el.value;
+        }
+        if (el.type === 'em') {
+            return '<em>' + el.value + '</em>';
+        }
+        if (el.type === 'strong') {
+            return '<strong>' + el.value + '</strong>';
+        }
+    })
+        .join('');
+}
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 var fb2 = require('./index');
 var md = require('./markdown');
+var html = require('./html');
 var fs = require('fs');
 
 var mdStream = fs
     .createReadStream('bible.u.fb2')
     .pipe(new fb2.Stream())
-    .pipe(new md());
+    .pipe(new html());
 
 mdStream.on('data', function(chunk) {
     console.log(chunk);
